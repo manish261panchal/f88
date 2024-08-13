822afc6407f7c057f4a02dd237d4912081304995
manish261panchal <manish@reclamedigital.com>
2024-08-13 13:59:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function startCaptcha() {
 
         // Redirect to the main site after a delay
         setTimeout(() => {
-            window.location.href = "UTM"; // Redirect URL
+            window.location.href = "https://www.gofun88.in/blog/plinko-game-rules-and-tips/"; // Redirect URL
         }, 1500); // 1.5 seconds delay before redirect
     }, 2000); // Simulated processing time (2 seconds)
 }
